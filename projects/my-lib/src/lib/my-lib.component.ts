dcebf1c704fc827935436ae58c1bfb22386bdefd
Mohamed Badr <mohamedbadr@Mohameds-MacBook-Pro.local>
2024-06-21 01:32:27 +0300

    init commit

---
+++ projects/my-lib/src/lib/my-lib.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-lib',
+  standalone: true,
+  imports: [],
+  template: ` <p>my-lib works!</p> `,
+  styles: ``,
+})
+export class MyLibComponent {}
