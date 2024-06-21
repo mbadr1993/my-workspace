5020aad4857ca26bf507381f500bdb687127d9cc
Mohamed Badr <mohamedbadr@Mohameds-MacBook-Pro.local>
2024-06-21 15:26:55 +0300

    add primeng

--- projects/my-lib/src/lib/my-lib.component.ts
+++ projects/my-lib/src/lib/my-lib.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'lib-my-lib',
   standalone: true,
-  imports: [],
-  template: ` <p>my-lib works!</p> `,
+  imports: [ButtonModule, InputTextModule],
+  template: `<p-button label="Click Me"></p-button>
+    <input pInputText type="text" placeholder="Enter text" />`,
   styles: ``,
 })
 export class MyLibComponent {}
